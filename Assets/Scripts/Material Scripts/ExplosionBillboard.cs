c739139d3934948c45e44cb77f0ca48e8f59af7e
Spex130 <Gamerguy56@bellsouth.net>
2018-08-13 13:32:41 -0400

    Functional Explosion

---
+++ Assets/Scripts/Material Scripts/ExplosionBillboard.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionBillboard : MonoBehaviour {
+
+
+    float speed = 1f;
+    public Renderer rend;
+    void Start()
+    {
+        rend = GetComponentInChildren<Renderer>();
+        rend.material.shader = Shader.Find("Custom/TimedExplosion");
+    }
+
+    void Update()
+    {
+        float pos = Mathf.PingPong(Time.time * speed, 1.1F);
+        rend.material.SetFloat("_Progression", pos);
+
+        if(pos >= 1)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}

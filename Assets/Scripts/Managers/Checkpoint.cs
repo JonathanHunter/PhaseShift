5f79ec625bc09779b8ad74ef3c94914e5d021d22
Spex130 <Gamerguy56@bellsouth.net>
2018-08-14 00:49:08 -0400

    Checkpoint Object

--- Assets/Scripts/Managers/Checkpoint.cs
+++ Assets/Scripts/Managers/Checkpoint.cs
@@ -10,6 +10,10 @@
 
         public Color activeColor;
 
+        public GameObject spinObject;
+
+        private bool active;
+
         private void Start()
         {
             rend = GetComponentInChildren<Renderer>();
@@ -29,9 +33,18 @@
                 CheckpointManager.Instance.RegisterCheckpoint(this.number);
                 this.col.enabled = false;
 
-                activeColor = Color.Lerp(activeColor, rend.material.GetColor("_GlowColor"), .005f);
-                rend.material.SetColor("_GlowColor", activeColor);
+                active = true;
 
+            }
+        }
+
+        private void Update()
+        {
+            spinObject.transform.Rotate(.5f, 1f, 1.5f);
+            if (active)
+            {
+                activeColor = Color.Lerp(activeColor, rend.material.GetColor("_GlowColor"), .001f);
+                rend.material.SetColor("_GlowColor", activeColor);
             }
         }
     }
